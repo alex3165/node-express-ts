912cde34dbc497dd0e20dafb0d4db2697b00d537
Alexandre Rieux <alexr.3165@gmail.com>
2017-06-12 09:23:45 +0100

    Update dependencies, introduce nodemon for watch mode

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 
 export default express.Router()
-  .use('/test', (req, res) => {
+  .use('/', (req, res) => {
     res.status(200).send({ hello: 'test' });
   });
